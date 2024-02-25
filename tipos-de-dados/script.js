f52ff380c715d63801444676c09a5f4ef00109d7
Rafiki <rafikidias.cy@gmail.com>
2024-02-25 02:08:23 -0300

    update exercício

--- tipos-de-dados/script.js
+++ tipos-de-dados/script.js
@@ -1,9 +1,10 @@
 function definir() {
-    let valorSigno = Number(document.getElementsByName("signo").value);
-    let valorTimeFutebol = Number(document.getElementsByName("time").value);
-    let valorEscolaridade = Number(document.getElementsByName("escolaridade").value);
-    let qtdeSoftskill = Number(document.getElementsByName("softskill").value);
+    let valorSigno = Number(document.querySelector('select[id="signo"]').value);
+    let valorTimeFutebol = Number(document.querySelector('select[id="time"]').value);
+    let valorEscolaridade = Number(document.querySelector('input[name="escolaridade"]:checked').value);
+    let qtdeSoftskill = Number(document.querySelectorAll('input[name="softskill"]:checked').length);
+    
 
     let resultado = valorSigno + valorTimeFutebol + valorEscolaridade + qtdeSoftskill;
-    document.getElementById(resultado).value = resultado
+    document.getElementById("resultado").value = resultado;
 }
